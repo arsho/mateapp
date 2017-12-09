34062468ddd4cb98bc0786aa6ef0e61699940641
arsho <shovon.sylhet@gmail.com>
2017-12-10 00:18:58 +0600

    updated webhook

--- app.py
+++ app.py
@@ -5,13 +5,14 @@ import traceback
 import random
 import os
 
+webhook_verify_token = os.environ.get('webhook_verify_token', None)
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET'])
 def verify():
   if request.args.get('hub.mode' == 'subscribe' and \
                       request.args.get('hub.challenge')):
-    if not request.args.get('hub.verify_token') == 'hello':
+    if not request.args.get('hub.verify_token') == webhook_verify_token:
       return 'verification token mismatch', 403
     return request.args['hub.challenge'], 200
   return 'hello mate', 200
